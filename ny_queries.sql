bcd58ee04348d57bdc05d2ffe1070d0404c14f3b
Jakub Kozanowski <kuba1016@gmail.com>
2021-10-22 12:18:24 +0100

    new_york_bikes_by_day_start_station_name -data source BigQuery

--- ny_queries.sql
+++ ny_queries.sql
@@ -19,6 +19,7 @@ ORDER BY
   date ASC
 
   -- new_york_bikes_by_day_start_station_name
+
   SELECT
     CAST (starttime AS date) AS date,
     start_station_name,
@@ -32,3 +33,25 @@ ORDER BY
     start_station_name
   ORDER BY
     date DESC
+
+    -- new_york_bikes_by_day_usertype_gender
+
+    SELECT
+      CAST(starttime AS date) AS date,
+      usertype,
+      gender,
+      COUNT(*) AS num_trips,
+      COUNT(DISTINCT bikeid) AS num_distinct_bikes,
+      ROUND(SUM(tripduration/60),2) AS total_trip_dration
+    FROM
+      `bigquery-public-data.new_york_citibike.citibike_trips`
+    WHERE
+      CAST(starttime AS date) IS NOT NULL
+      AND gender IN("male",
+        "female")
+    GROUP BY
+      date,
+      usertype,
+      gender
+    ORDER BY
+      date ASC
